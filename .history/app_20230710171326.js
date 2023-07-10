7ed5b0ab525fb40a528466a674691e6194a4f073
ben <yitezheng13@gmail.com>
2023-07-11 03:04:33 +0800

    First commit.

---
+++ .history/app_20230710171326.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/wikiDB");
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
---
+++ .history/app_20230711020935.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
+app.get("/register", (req, res) => {
+    res.render("home");
+})
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save().then(() =>{
+        res.render("secrets");
+    });
+})
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
